3167ec755bcd4becf4d9e7bcbee2dac583baa707
Alief Mumtaz <86474503+aliefmumtaz@users.noreply.github.com>
2022-06-27 13:31:15 +0800

    update shared

--- lib/shared/shared.dart
+++ lib/shared/shared.dart
@@ -3,17 +3,4 @@ import 'package:google_fonts/google_fonts.dart';
 
 part 'theme.dart';
 
-const String mapAPI = 'AIzaSyBqsUaFRdkf9rPtSh1GP2pXVZ6lCnlOohQ';
-// const String baseURL = 'http://13.250.236.249:5011/';
-const String baseURL = 'http://52.74.116.140:5011/';
-const String staticToken =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIzM2I5YWRiNDI2M2JiMTBlYjgzZjNhODdhN2Q0IiwiZ3JvdXBfaWQiOjI2MDgzLCJpYXQiOjE2MjMxMzA1MDB9.Nn9PMGzaiTYDPjclTPsdqjdAudJw8r_D3ItOkLq2N7U';
-const String getUserPointToken =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIzM2I5YWRiNDI2M2JiMTBlYjgzZjNhODdhN2Q0IiwiZ3JvdXBfaWQiOjI2MDgzLCJpYXQiOjE2MjMxMzA1MDB9.Nn9PMGzaiTYDPjclTPsdqjdAudJw8r_D3ItOkLq2N7U';
-const String oneSignalAppId = '4f3ed19a-7a83-49c6-b94e-7f5603c8bfdc';
-const String imgNotifUrl =
-    "https://firebasestorage.googleapis.com/v0/b/yotta-app-3f95f.appspot.com/o/logo.png?alt=media&token=25fd8626-cd64-41ab-9e0b-74f015740928";
-
-const String yottaServerUrl = 'http://api2.yotta.co.id';
-
 const String appVersion = '2.2.1';
